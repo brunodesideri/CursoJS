fb9c2d8d520b90462dd8fffb8938819c395f2531
Bruno Desideri <150479044+brunodesideri@users.noreply.github.com>
2024-03-08 21:32:28 -0300

    Update ex011.js

--- aula012/ex011.js
+++ aula012/ex011.js
@@ -1,6 +1,10 @@
-var idade = 12
-if (idade < 18) {
+var idade = 66
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
     console.log('Não vota')
-} else {
-    console.log('Vota')
+} else if (idade < 18 || idade > 65){
+        console.log('Você pode votar, mas não é obrigatório')
+} else if (idade < 67) {
+        console.log('Voto obrigatório!')
 }
+
