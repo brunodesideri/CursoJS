5e332eeb8cbe2fe5a012f9f8515aa66c9bca9723
Bruno Desideri <150479044+brunodesideri@users.noreply.github.com>
2024-03-11 20:55:36 -0300

    ex js

---
+++ aula015/ambiente.js
@@ -0,0 +1,18 @@
+let num = [5,8,2,9,3]
+num.push(7)
+num.sort()
+console.log (num)
+console.log (`O vetor tem ${num.length} posições`)
+console.log (`O primeiro valor do vetor é ${num[0]}`)
+for (pos = 0; pos < num.length; pos++ ) {
+    console.log(num[pos])    
+}
+console.log('-^')
+for (var pos in num) {
+    console.log(num[pos])
+}
+
+let position = num.indexOf(8)
+
+console.log('-^')
+console.log(position)
