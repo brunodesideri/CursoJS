43e9ce44e464b68d9a2de56eff8926dadfdece25
Bruno Desideri <150479044+brunodesideri@users.noreply.github.com>
2024-03-10 21:57:07 -0300

    ex

--- aula013/ambiente.js
+++ aula013/ambiente.js
@@ -1,5 +1,10 @@
-var c = 1
-while (c < 6) {
-    console.log('Tudo bem?')
+do {
+    console.log(`Passo ${c}`)
     c ++
-}
+} while  (c <= 10)
+
+/* var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c ++
+} */
